d306a1b0712a5ab69b791184a3d44322a2092cb3
kalen510 <kalen.wiley@gmail.com>
2023-05-03 18:38:35 -0400

    post endpoint fixes #2

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals.json');
 
 // use port sent by heroku if provided, if not, use port 3001
@@ -6,6 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 // 1: instantiate the server
 const app = express();
+
+// MIDDLEWARE: intercept POST request and convert raw data to JSON object
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parese incoming JSON data
+app.use(express.json());
 
 // filter results based on query strings
 function filterByQuery(query, animalsArray) {
@@ -44,6 +52,37 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    // update our local copy of the animal data (animals)
+    animalsArray.push(animal);
+
+    // write new array to our json file
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
 // 3: create a route that the frontend can request data from 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -66,7 +105,24 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+// POST route represents action of client requesting the server to accept data
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error bacl
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
+});
+
 // 2: chain listen() onto our server to make our server listen for requests
+// this can go anywhere after the instatiation of our server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
