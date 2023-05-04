4b539f3a4f583009d238b1fd341daf3942d54fd3
kalen510 <kalen.wiley@gmail.com>
2023-05-04 00:24:40 -0400

    create zookeeper api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
+const animalRoutes = require('./animalRoutes');
+const zookeeperRoutes = require('./zookeeperRoutes');
 
 // middleware
 router.use(animalRoutes);
+router.use(zookeeperRoutes);
 
 module.exports = router;
